c681294f7840b40f23caa7c48824abc814660063
*dJdCucEGeo <msipod3394@gmail.com>
2024-04-26 15:40:20 +0900

    一旦ファイル名を変更した可能性のあるディレクトリを削除

--- src/components/atoms/LoadingIndicator.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export function LoadingIndicator() {
-  return <p>読み込み中</p>
-}
--- src/components/molecules/ItemCardList.tsx
+++
@@ -1,7 +0,0 @@
-import { memo } from 'react'
-import { ItemCard } from './ItemCard'
-import { ItemWithCount } from '@/types/ItemWithCount'
-
-export const ItemCardList = memo(({ items }: { items: ItemWithCount[] }) => {
-  return items.map((item: ItemWithCount) => <ItemCard key={item.id} item={item} />)
-})
