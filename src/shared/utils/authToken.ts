25b68e160988d17707677e6be0901e0a73a738c1
*dJdCucEGeo <msipod3394@gmail.com>
2024-04-26 15:52:21 +0900

    authToken ローカルストレージが利用可能かの判定を追加

--- src/shared/utils/authToken.ts
+++ src/shared/utils/authToken.ts
@@ -1,5 +1,20 @@
-const  key = "authToken"
+const key = 'authToken'
 
-export const getStoredAuthToken = () => localStorage.getItem(key)
-export const storeAuthToken = (token: string) => localStorage.setItem(key, token)
-export const removeStoredAuthToken = () => localStorage.removeItem(key)
+export const getStoredAuthToken = () => {
+  if (typeof window !== 'undefined') {
+    return localStorage.getItem(key)
+  }
+  return null
+}
+
+export const storeAuthToken = (token: string) => {
+  if (typeof window !== 'undefined') {
+    localStorage.setItem(key, token)
+  }
+}
+
+export const removeStoredAuthToken = () => {
+  if (typeof window !== 'undefined') {
+    localStorage.removeItem(key)
+  }
+}
